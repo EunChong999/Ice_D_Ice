f4c00c4f10451d581bcf4f1bf0b305e45446c674
EunChong999 <eunchonggim999@gmail.com>
2024-01-19 23:19:24 +0900

    feat : 주사위 회전 입력 간소화

--- Assets/EunChong/Scripts/Dice.cs
+++ Assets/EunChong/Scripts/Dice.cs
@@ -31,6 +31,65 @@ public class Dice : MonoBehaviour
 
     private void Update()
     {
+        CheckHiddenFaces();
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            InitState();
+
+            RegulateDamping(2);
+        }
+
+        if (!isMoving)
+        {
+            if (!canReveal)
+            {
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    StartCoroutine(CoolTime());
+                    canReveal = true;
+                }
+            }
+
+            if (isRevealing)
+            {
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    hiddenFaces.SetActive(true);
+
+                    RegulateDamping(1);
+                }
+            }
+
+            if (Input.GetKey(KeyCode.W) && checkers[0].isCollisioning) // Z축으로 움직일 때
+            {
+                StartCoroutine(Roll(Vector3.forward));
+                ChangeAxis(ref yPoints, ref zPoints, ref xPoints);
+            }
+            else if (Input.GetKey(KeyCode.A) && checkers[1].isCollisioning) // X축으로 움직일 때
+            {
+                StartCoroutine(Roll(Vector3.left));
+                ChangeAxis(ref xPoints, ref yPoints, ref zPoints);
+            }
+            else if (Input.GetKey(KeyCode.S) && checkers[2].isCollisioning) // Z축으로 움직일 때
+            {
+                StartCoroutine(Roll(Vector3.back));
+                ChangeAxis(ref yPoints, ref zPoints, ref xPoints);
+            }
+            else if (Input.GetKey(KeyCode.D) && checkers[3].isCollisioning) // X축으로 움직일 때
+            {
+                StartCoroutine(Roll(Vector3.right));
+                ChangeAxis(ref xPoints, ref yPoints, ref zPoints);
+            }
+        }
+        else
+        {
+            InitState();
+        }
+    }
+
+    void CheckHiddenFaces()
+    {
         foreach (var x in xPoints)
         {
             if (x.isHidden)
@@ -54,70 +113,20 @@ public class Dice : MonoBehaviour
                 hiddenImages[0].sprite = diceFaces[z.num - 1];
             }
         }
+    }
 
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            hiddenFaces.SetActive(false);
-            canReveal = false;
-            isRevealing = false;
-            virtualCamera.GetCinemachineComponent<CinemachineTransposer>().m_XDamping = 2;
-            virtualCamera.GetCinemachineComponent<CinemachineTransposer>().m_YDamping = 2;
-            virtualCamera.GetCinemachineComponent<CinemachineTransposer>().m_ZDamping = 2;
-        }
+    void InitState()
+    {
+        hiddenFaces.SetActive(false);
+        canReveal = false;
+        isRevealing = false;
+    }
 
-        if (!isMoving)
-        {
-            if (!canReveal)
-            {
-                if (Input.GetKey(KeyCode.Space))
-                {
-                    StartCoroutine(CoolTime());
-                    canReveal = true;
-                }
-            }
-
-            if (isRevealing)
-            {
-                if (Input.GetKey(KeyCode.Space))
-                {
-                    hiddenFaces.SetActive(true);
-                    virtualCamera.GetCinemachineComponent<CinemachineTransposer>().m_XDamping = 1;
-                    virtualCamera.GetCinemachineComponent<CinemachineTransposer>().m_YDamping = 1;
-                    virtualCamera.GetCinemachineComponent<CinemachineTransposer>().m_ZDamping = 1;
-                }
-            }
-
-            if (Input.GetKey(KeyCode.W) && checkers[0].isCollisioning) // Z축으로 움직일 때
-            {
-                StartCoroutine(Roll(Vector3.forward));
-
-                ChangeYZAxis();
-            }
-            else if (Input.GetKey(KeyCode.A) && checkers[1].isCollisioning) // X축으로 움직일 때
-            {
-                StartCoroutine(Roll(Vector3.left));
-
-                ChangeXYAxis();
-            }
-            else if (Input.GetKey(KeyCode.S) && checkers[2].isCollisioning) // Z축으로 움직일 때
-            {
-                StartCoroutine(Roll(Vector3.back));
-
-                ChangeYZAxis();
-            }
-            else if (Input.GetKey(KeyCode.D) && checkers[3].isCollisioning) // X축으로 움직일 때
-            {
-                StartCoroutine(Roll(Vector3.right));
-
-                ChangeXYAxis();
-            }
-        }
-        else
-        {
-            hiddenFaces.SetActive(false);
-            canReveal = false;
-            isRevealing = false;
-        }
+    void RegulateDamping(float damping)
+    {
+        virtualCamera.GetCinemachineComponent<CinemachineTransposer>().m_XDamping = damping;
+        virtualCamera.GetCinemachineComponent<CinemachineTransposer>().m_YDamping = damping;
+        virtualCamera.GetCinemachineComponent<CinemachineTransposer>().m_ZDamping = damping;
     }
 
     IEnumerator CoolTime()
@@ -128,12 +137,14 @@ public class Dice : MonoBehaviour
 
     IEnumerator Roll(Vector3 direction)
     {
+        // 움직이고 있다.
         isMoving = true;
 
         float remainingAngle = 90;
         Vector3 rotationCenter = transform.position + direction / 2 + Vector3.down / 2;
         Vector3 rotationAxis = Vector3.Cross(Vector3.up, direction);
 
+        // 남은 각도가 없을 때까지 계속 회전시킨다.
         while (remainingAngle > 0) 
         {
             float rotationAngle = Mathf.Min(Time.deltaTime * speed, remainingAngle);
@@ -142,60 +153,48 @@ public class Dice : MonoBehaviour
             yield return null;
         }
 
+        // 위치 조정을 위한 반올림을 한다.
         Round();
 
+        // 움직이고 있지 않다.
         isMoving = false;
     }
 
     void Round()
     {
-        // 현재 오브젝트의 포지션을 첫 번째 소수점에서 반올림합니다.
+        // 현재 오브젝트의 포지션을 첫 번째 소수점에서 반올림한다.
         Vector3 roundedPosition = new Vector3(
             Mathf.Round(transform.position.x * 10.0f) / 10.0f,
             Mathf.Round(transform.position.y * 10.0f) / 10.0f,
             Mathf.Round(transform.position.z * 10.0f) / 10.0f
         );
 
-        // 반올림된 포지션을 현재 오브젝트에 적용합니다.
+        // 반올림된 포지션을 현재 오브젝트에 적용한다.
         transform.position = roundedPosition;
 
-        // 현재 오브젝트의 회전을 첫 번째 소수점에서 반올림합니다.
+        // 현재 오브젝트의 회전을 첫 번째 소수점에서 반올림한다.
         Vector3 roundedRotation = new Vector3(
             Mathf.Round(transform.rotation.eulerAngles.x * 10.0f) / 10.0f,
             Mathf.Round(transform.rotation.eulerAngles.y * 10.0f) / 10.0f,
             Mathf.Round(transform.rotation.eulerAngles.z * 10.0f) / 10.0f
         );
 
-        // 반올림된 회전을 현재 오브젝트에 적용합니다.
+        // 반올림된 회전을 현재 오브젝트에 적용한다.
         transform.rotation = Quaternion.Euler(roundedRotation);
     }
 
-    void ChangeXYAxis()
+    void ChangeAxis(ref Point[] firstPoints, ref Point[] secondPoints, ref Point[] thirdPoints)
     {
-        Point[] xPointTemps = xPoints;
-        Point[] yPointTemps = yPoints;
+        Point[] firstPointTemps = firstPoints;
+        Point[] secondPointTemps = secondPoints;
 
-        foreach (var zPoint in zPoints)
+        foreach (var thirdPoint in thirdPoints)
         {
-            zPoint.isShapeChanged = !zPoint.isShapeChanged;
+            thirdPoint.isShapeChanged = !thirdPoint.isShapeChanged;
         }
 
-        xPoints = yPointTemps;
-        yPoints = xPointTemps;
-    }
-
-    void ChangeYZAxis()
-    {
-        Point[] yPointTemps = yPoints;
-        Point[] zPointTemps = zPoints;
-
-        foreach (var xPoint in xPoints)
-        {
-            xPoint.isShapeChanged = !xPoint.isShapeChanged;
-        }
-
-        yPoints = zPointTemps;
-        zPoints = yPointTemps;
+        firstPoints = secondPointTemps;
+        secondPoints = firstPointTemps;
     }
 }
 
