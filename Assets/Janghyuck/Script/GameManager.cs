4c519989805946a078292bfac986f13b43fdfb68
EunChong999 <eunchonggim999@gmail.com>
2024-02-25 15:35:58 +0900

    feat : 스프라이트 명도 변경

--- Assets/Janghyuck/Script/GameManager.cs
+++ Assets/Janghyuck/Script/GameManager.cs
@@ -6,17 +6,5 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public void Update()
-    {
-        Exit();
-    }
 
-    //게임 자체 종료
-    public void Exit()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Application.Quit();
-        }
-    }
 }
