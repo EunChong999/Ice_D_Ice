ddc0502f6f2ec250e7fe95dbd6e130e57f074062
EunChong999 <eunchonggim999@gmail.com>
2024-03-01 18:10:23 +0900

    feat : 장애물이 주사위를 가릴 때 장애물을 투명하게 변하도록 함

---
+++ Assets/EunChong/Scripts/ObjectFader.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFader : MonoBehaviour
+{
+    public float fadeSpeed;
+
+    [Range(0, 1)]
+    public float fadeAmount;
+
+    [Range(0, 1)]
+    public float originOpacity;
+
+    Material[] materials = new Material[4];
+    public bool doFade = false;
+
+    private void Start()
+    {
+        for (int i = 0; i < GetComponent<Renderer>().materials.Length; i++) 
+        {
+            materials[i] = GetComponent<Renderer>().materials[i];
+        }
+    }
+
+    private void Update()
+    {
+        if (doFade)
+            FadeNow();
+        else
+            ResetFade();
+    }
+
+    void FadeNow()
+    {
+        foreach (Material mat in materials)
+        {
+            if (mat != null)
+            {
+                Color currentColor = mat.color;
+                Color smoothColor = new Color(currentColor.r, currentColor.g, currentColor.b,
+                    Mathf.Lerp(currentColor.a, fadeAmount, fadeSpeed * Time.deltaTime));
+                mat.color = smoothColor;
+            }
+        }
+    }
+
+    void ResetFade()
+    {
+        foreach (Material mat in materials)
+        {
+            if (mat != null)
+            {
+                Color currentColor = mat.color;
+                Color smoothColor = new Color(currentColor.r, currentColor.g, currentColor.b,
+                    Mathf.Lerp(currentColor.a, originOpacity, fadeSpeed * Time.deltaTime));
+                mat.color = smoothColor;
+            }
+        }
+    }
+}
