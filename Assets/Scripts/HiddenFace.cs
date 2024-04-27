a380a52efb8baba03635d7c560edbd815d7f3611
EunChong999 <eunchonggim999@gmail.com>
2024-04-27 09:26:08 +0900

    feat : 여러가지 개선

---
+++ Assets/Scripts/HiddenFace.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiddenFace : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    Vector3 originPos;
+    [SerializeField] float speed;
+    Vector3 velocity = Vector3.zero;
+
+    private void Start()
+    {
+        originPos = transform.position;
+    }
+
+    void OnEnable()
+    {
+        StartCoroutine(move());
+    }
+
+    void OnDisable()
+    {
+        transform.position = originPos;
+    }
+
+    IEnumerator move()
+    {
+        while (transform.position != target.position)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, target.position, ref velocity, speed);
+            yield return null;
+        }
+    }
+
+}
